1ad5b8d141914012e315c1a088d2299049f0770a
medrimonia <medrimonia@gmail.com>
2016-05-03 17:09:02 +0900

    Initial commit (CMake + Multivariate gaussian)

---
+++ src/rosban_gp/multivariate_gaussian.cpp
@@ -0,0 +1,37 @@
+#include "rosban_gp/multivariate_gaussian.h"
+
+#include <Eigen/Cholesky>
+
+namespace rosban_gp
+{
+
+MultiVariateGaussian::MultiVariateGaussian()
+{
+}
+
+MultiVariateGaussian::MultiVariateGaussian(const Eigen::VectorXd & mu_,
+                                           const Eigen::MatrixXd & sigma_)
+  : mu(mu_), sigma(sigma_)
+{
+  updateCholesky();
+}
+
+Eigen::VectorXd MultiVariateGaussian::getSample(std::default_random_engine & engine) const
+{
+  Eigen::VectorXd random_vector(mu.rows());
+  std::normal_distribution<double> distribution(0,1);
+  for (int dim = 0; dim < mu.rows(); dim++)
+  {
+    random_vector(dim) = distribution(engine);
+  }
+  return mu + cholesky * random_vector;
+}
+
+void MultiVariateGaussian::updateCholesky()
+{
+  //WARNING: according to Rasmussen 2006, it might be necessary to add epsilon * I
+  //         before computing cholesky
+  cholesky = Eigen::LLT<Eigen::MatrixXd>(sigma).matrixL();
+}
+
+}

f6093e1a29ef682b3db238f9648028598a99bb35
medrimonia <medrimonia@gmail.com>
2016-05-10 17:41:52 +0900

    Observation generator

--- src/exercises/testGradient.cpp
+++ src/exercises/testGradient.cpp
@@ -15,6 +15,47 @@ std::default_random_engine get_random_engine()
   return std::default_random_engine(seed);
 }
 
+/// measurement_noise is std_dev
+/// for inputs:
+/// each col is a different input
+/// each row represent a different dimension
+Eigen::VectorXd generateObservations(const Eigen::MatrixXd & inputs,
+                                     std::function<double(const Eigen::VectorXd &)> f,
+                                     double measurement_noise)
+{
+  auto engine = get_random_engine();
+
+  std::normal_distribution<double> noise_distrib(0, measurement_noise);
+
+  Eigen::VectorXd observations(inputs.cols());
+
+  for (int i = 0; i < observations.size(); i++)
+  {
+    observations(i) = f(inputs.col(i)) + noise_distrib(engine);
+  }
+
+  return observations;
+}
+
+/// easy mapping
+/// measurement_noise is std_dev
+/// for inputs:
+/// each col is a different input
+/// each row represent a different dimension
+Eigen::VectorXd generateObservations(const Eigen::MatrixXd & inputs,
+                                     std::function<double(double)> f,
+                                     double measurement_noise)
+{
+  return generateObservations(inputs,
+                              [f](const Eigen::VectorXd & input)
+                              {
+                                return f(input(0));
+                              },
+                              measurement_noise);
+}
+                                     
+
+
 /// This code uses a very simple gradient ascent to find the maximum of the parameters
 int main(int argc, char ** argv)
 {
@@ -44,13 +85,29 @@ int main(int argc, char ** argv)
   generative_gp.setCovarFunc(std::move(generative_func));
   generative_gp.setMeasurementNoise(sn);
   Eigen::VectorXd observations = generative_gp.generateValues(inputs, engine, true);
+  // tmp override
+  //observations = generateObservations(inputs, [](double x)
+  //                                    {
+  //                                      return
+  //                                      0.01 * x * x * x * x
+  //                                      - 0.2 * x * x * x
+  //                                      + 2 * x * x
+  //                                      +  3 * x
+  //                                      - 2;
+  //                                    }, 0.05);
+  observations = generateObservations(inputs, [](double x)
+                                      {
+                                        if (x > 2) return 1;
+                                        return -1;
+                                      }, 0.05);
 
   // Optimizing using gradient ascent
   Eigen::VectorXd guess, last_guess;
   guess      = Eigen::VectorXd::Constant(3,1);
   last_guess = Eigen::VectorXd::Constant(3,0);
   double epsilon = std::pow(10, -6);
-  double gamma = 0.0001;
+  Eigen::VectorXd gamma(3);
+  gamma << std::pow(10, -5), std::pow(10,-2), std::pow(10,-2);
 
   GaussianProcess gp(inputs, observations,
                      std::unique_ptr<CovarianceFunction>(new SquaredExponential()));
@@ -62,15 +119,15 @@ int main(int argc, char ** argv)
     std::cout << "guess: " << guess.transpose() << std::endl
               << "\tgradient: " << gradient.transpose() << std::endl;
     // Hack: avoid to get out of limits
-    double gain = gamma;
-    for (int i = 0; i < gradient.rows(); i++)
-    {
-      if (last_guess(i) + gradient(i) * gain < 0)
-      {
-        gain = - last_guess(i) / gradient(i);
-      }
-    }
-    guess = last_guess + gain * gradient;
+    Eigen::VectorXd gain = gamma;
+    //for (int i = 0; i < gradient.rows(); i++)
+    //{
+    //  if (last_guess(i) + gradient(i) * gain(i) < 0)
+    //  {
+    //    gain(i) = - last_guess(i) / gradient(i);
+    //  }
+    //}
+    guess = last_guess + gain.cwiseProduct(gradient);
   }
 
   // Use final guess

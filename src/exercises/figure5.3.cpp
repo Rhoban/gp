caf9cf9347b6708b80b04a72c6022b2730f7b78e
medrimonia <medrimonia@gmail.com>
2016-05-10 12:24:17 +0900

    Producing data for fig5.3.b (not plotted yet)

---
+++ src/exercises/figure5.3.cpp
@@ -0,0 +1,69 @@
+#include "rosban_gp/multivariate_gaussian.h"
+#include "rosban_gp/core/gaussian_process.h"
+#include "rosban_gp/core/squared_exponential.h"
+
+#include <functional>
+#include <fstream>
+#include <chrono>
+
+using namespace rosban_gp;
+
+std::default_random_engine get_random_engine()
+{
+  unsigned long seed = std::chrono::system_clock::now().time_since_epoch().count();
+  return std::default_random_engine(seed);
+}
+
+/// This code produces figure 5.3.b at page 113 in Rasmussen-2006
+int main(int argc, char ** argv)
+{
+  // Setting problem properties
+  double x_min = -8;
+  double x_max = 8;
+  // Parameters
+  int nb_points = 21;
+  double length_scale = 1;
+  double sn = 0.1;
+  double sf = 1;
+
+  auto engine = get_random_engine();
+
+  // Generating inputs
+  std::uniform_real_distribution<double> input_distrib(x_min, x_max);
+  Eigen::MatrixXd inputs(1,nb_points);
+  for (int p = 0; p < nb_points; p++)
+  {
+    inputs(0,p) = input_distrib(engine);
+  }
+
+  // Generating observations
+  std::unique_ptr<CovarianceFunction> generative_func(new SquaredExponential(length_scale, sf));
+  GaussianProcess generative_gp;
+  generative_gp.setCovarFunc(std::move(generative_func));
+  generative_gp.setMeasurementNoise(sn);
+  Eigen::VectorXd observations = generative_gp.generateValues(inputs, engine);
+
+  std::ofstream out;
+  out.open("fig_5_3_b.csv");
+
+  out << "lengthScale,logMarginalLikelihood" << std::endl;
+
+  // Generating data
+  int nb_plot_points = 1000;
+  double min_l_exp = -5;
+  double max_l_exp =  1;
+  for (int point = 0; point < nb_plot_points; point++)
+  {
+    double delta = max_l_exp - min_l_exp;
+    double exp = min_l_exp + delta *  point / nb_plot_points;
+    double l = std::pow(10, exp);
+    // Create GP with the given parameter
+    std::unique_ptr<CovarianceFunction> f(new SquaredExponential(l, sf));
+    GaussianProcess gp(inputs, observations, std::move(f));
+    gp.setMeasurementNoise(sn);
+
+    out << l << "," << gp.getLogMarginalLikelihood() << std::endl;
+  }
+
+  out.close();
+}

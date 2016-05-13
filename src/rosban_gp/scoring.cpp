7c2504c65037363b27672eeff0f05e9e6ce32090
medrimonia <medrimonia@gmail.com>
2016-05-13 22:44:20 +0900

    Add Scoring with MSE and SME

---
+++ src/rosban_gp/scoring.cpp
@@ -0,0 +1,32 @@
+#include "rosban_gp/scoring.h"
+
+namespace rosban_gp
+{
+
+double computeMSE(const Eigen::VectorXd & observations,
+                  const Eigen::VectorXd & predictions)
+{
+  return (observations - predictions).squaredNorm() / predictions.rows();
+}
+
+/// Compute the standardized mean squared error
+/// SMSE = MSE / Var(observations) 
+double computeSMSE(const Eigen::VectorXd & observations,
+                   const Eigen::VectorXd & predictions)
+{
+  mean_obs = predictions.sum() / predictions.rows();
+  double var = (predictions - mean_obs).squaredNorm();
+  return computeMSE(observations, predictions) / var;
+}
+
+
+/// Compute the mean standardized log loss
+/// MSLL = avg(SLL)
+double computeMSLL(const Eigen::MatrixXd & inputs,
+                   const Eigen::VectorXd & observations,
+                   const Eigen::VectorXd & predictions)
+{
+  throw std::runtime_error("unimplemented function");
+}
+
+}

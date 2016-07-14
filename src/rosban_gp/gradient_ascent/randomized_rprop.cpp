0a32d545e3bbf4c2cf75b024751069037a26a4c2
medrimonia <medrimonia@gmail.com>
2016-07-14 12:44:03 +0900

    Using logSpace to draw initial guesses and initial step sizes in randomized rprop (untested)

--- src/rosban_gp/gradient_ascent/randomized_rprop.cpp
+++ src/rosban_gp/gradient_ascent/randomized_rprop.cpp
@@ -30,6 +30,9 @@ void RandomizedRProp::Config::from_xml(TiXmlNode *node)
   rprop_conf->tryRead(node, "rprop_conf");
 }
 
+// NOTE: implementation of tuning space is quite weird in order to avoid
+//       breaking API.
+// TODO: Think about another way once tests have been led
 Eigen::VectorXd RandomizedRProp::run(RProp::GradientFunc gradient_func,
                                      ScoringFunc scoring_func,
                                      const Eigen::MatrixXd & limits,
@@ -40,24 +43,37 @@ Eigen::VectorXd RandomizedRProp::run(RProp::GradientFunc gradient_func,
   //          << "\tnb_trials     : " << conf.nb_trials      << std::endl
   //          << "\tmax_iterations: " << conf.rprop_conf->max_iterations << std::endl
   //          << "\tepsilon       : " << conf.rprop_conf->epsilon        << std::endl;
-  // Defining minimal and maximal initial step sizes
+
+  // Get limits inside tuning space
+  Eigen::MatrixXd tuning_space_limits = limits;
+  tuning_space_limits.col(0) = RProp::cvtToTuningSpace(limits.col(0), conf.rprop_conf);
+  tuning_space_limits.col(1) = RProp::cvtToTuningSpace(limits.col(1), conf.rprop_conf);
+
+  // Defining minimal and maximal initial step sizes (in tuning_space
   Eigen::MatrixXd step_size_limits(limits.rows(), limits.cols());
   step_size_limits.col(0) = Eigen::VectorXd::Constant(limits.rows(), conf.rprop_conf->epsilon);
-  step_size_limits.col(1) = (limits.col(1) - limits.col(0) ) / 100;//100 should go to a variable
-  // Creating random initial guesses and random initial steps
+  // Max is 1/100th of the total range
+  step_size_limits.col(1) = (tuning_space_limits.col(1) - tuning_space_limits.col(0) ) / 100;
+  // Creating random initial guesses and random initial steps (in tuning space)
   Eigen::MatrixXd initial_guesses;
   Eigen::MatrixXd initial_step_sizes;
-  initial_guesses    = rosban_random::getUniformSamplesMatrix(limits, conf.nb_trials);
+  initial_guesses    = rosban_random::getUniformSamplesMatrix(tuning_space_limits, conf.nb_trials);
   initial_step_sizes = rosban_random::getUniformSamplesMatrix(step_size_limits, conf.nb_trials);
   // Preparing common data
   double best_value = std::numeric_limits<double>::lowest();
   Eigen::VectorXd best_guess = (limits.col(0) + limits.col(1)) / 2;
   // Running several rProp optimization with different starting points
   for (int trial = 0; trial < conf.nb_trials; trial++) {
+    Eigen::VectorXd initial_guess, initial_step_size, tmp;
+    // Going back to original space for legacy reasons: QUICK AND DIRTY
+    initial_guess = RProp::cvtFromTuningSpace(initial_guesses.col(trial), conf.rprop_conf);
+    tmp = initial_guesses.col(trial) + initial_step_sizes.col(trial);
+    initial_step_size = RProp::cvtFromTuningSpace(tmp, conf.rprop_conf) - initial_guess;
+    // Computing best_guess
     Eigen::VectorXd current_guess;
     current_guess = RProp::run(gradient_func,
-                               initial_guesses.col(trial),
-                               initial_step_sizes.col(trial),
+                               initial_guess,
+                               initial_step_size,
                                limits,
                                conf.rprop_conf);
     double value = scoring_func(current_guess);
